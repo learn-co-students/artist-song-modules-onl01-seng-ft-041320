af01c4e996c323d89426847ff17760521317630d
Devone Austin <devone.austin@gmail.com>
2020-04-30 01:50:49 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,21 @@
+
+#Class methods for Artist and Song
+
+module Memorable
+  module ClassMethods
+
+    def reset_all
+      all.clear
+    end
+
+    def count
+      all.count
+    end
+  end
+
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
